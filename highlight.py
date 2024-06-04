32bc09789adc79bc63a9fc7adb362abf12141495
Jaden You <youj2005@gmail.com>
2024-06-04 10:22:10 -0400

    Model Train

--- highlight.py
+++ highlight.py
@@ -1,13 +1,25 @@
 import torch.nn as nn
-from transformers import T5Config
+import torch
+from transformers import AutoConfig, AutoModel
 
 class HighlightHead(nn.Module):
-    def __init__(self, outdim: int, config: T5Config = T5Config.from_pretrained('t5-small')):
+    
+    def __init__(self, outdim: int, model_name: str = 'bert-base-cased'):
         super().__init__()
-        self.linear = nn.Linear(config.d_model, outdim, bias=True)
-        self.softmax = nn.Softmax(dim=0)
+        self.outdim = outdim
+        self.config = AutoConfig.from_pretrained(model_name)
+        self.model = AutoModel.from_pretrained(model_name)
+        self.dropout = nn.Dropout(0.3)
+        self.linear = nn.Linear(self.config.hidden_size, outdim, bias=False)
+        self.logsoftmax = nn.LogSoftmax(dim=1)
+        self.loss_fn = nn.NLLLoss()
         
-    def forward(self, input_ids):
-        output = self.linear(input_ids)
-        logits = self.softmax(output)
-        return logits
+    def forward(self, input_ids, target, attention_mask):
+        encoded = self.model(input_ids, token_type_ids=None, attention_mask=attention_mask)
+        encoded = torch.squeeze(encoded['last_hidden_state'])
+        d_encoded = self.dropout(encoded)
+        outputs = self.linear(d_encoded)
+        probs = self.logsoftmax(outputs)
+        target_attn = torch.where(attention_mask == 1, target, self.loss_fn.ignore_index).squeeze()
+        loss = self.loss_fn(probs, target_attn)
+        return probs, loss
